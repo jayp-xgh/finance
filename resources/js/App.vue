791bfb3a2262038f5e4c552219121a86e2b1a6d3
jayp <joaovitorpereiramartins.2004@gmail.com>
2024-09-15 02:40:45 -0300

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+<!--        <Header />-->
+<!--        <Sidebar />-->
+        <MenuBar />
+        <FixedIncomeCalculator />
+        <router-view></router-view>
+    </div>
+</template>
+
+<script setup>
+    import Sidebar from '../js/components/Sidebar.vue';
+    import Header from '../js/components/Header.vue';
+    import MenuBar from '@/components/MenuBar.vue';
+    import FixedIncomeCalculator from "@/components/FixedIncomeCalculator.vue";
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2 class="text-amber-600">Home Page</h2>
+    </div>
+</template>
+
